d27a1af00ab48051eb088db0462f0a4e451f5c37
Meliodas-dragon-wrath <57758881+Meliodas-dragon-wrath@users.noreply.github.com>
2020-11-12 21:57:44 +0530

    Update models.py

--- Sports_Management_System/Events/models.py
+++ Sports_Management_System/Events/models.py
@@ -8,8 +8,7 @@ class event(models.Model):
     organiser = models.CharField(max_length=200)
     temp = 'csvfile_for_guest/' + str(id) + '/'
     csvfile = models.FileField(upload_to=temp)
-
-
+    
     def __str__(self):
         return self.name
 
