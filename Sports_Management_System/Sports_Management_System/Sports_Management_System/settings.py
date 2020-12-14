d4a98d4234c0fc3b6928632336130a008619b9aa
Samyak Jain <lit2018056@iiitl.ac.in>
2020-12-14 23:24:26 +0530

    Update settings.py
    For privacy concern

--- Sports_Management_System/Sports_Management_System/Sports_Management_System/settings.py
+++ Sports_Management_System/Sports_Management_System/Sports_Management_System/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '1008jainsamyak1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Jainsamyak@1'
+EMAIL_HOST_USER = 'write your email here'
+EMAIL_HOST_PASSWORD = 'your password here'
